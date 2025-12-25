384030e9f715bed56535523c25778cb4901dcc39
Azhar0121 <azharathallah338@gmail.com>
2025-12-25 07:27:26 +0700

    model dan api client

---
+++ src/api/InventarisApiClient.java
@@ -0,0 +1,105 @@
+package api;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.HashMap;
+import java.util.List;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import model.Inventaris;
+
+public class InventarisApiClient {
+
+    private static final String BASE_URL =
+            "http://localhost/realtime-application-tier-php/public/inventaris";
+
+    private final HttpClient client = HttpClient.newHttpClient();
+    private final Gson gson = new Gson();
+
+    public List<Inventaris> findAll() throws Exception {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(BASE_URL))
+                .GET()
+                .build();
+
+        HttpResponse<String> response =
+                client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        ApiResponse<List<Inventaris>> apiResp =
+                gson.fromJson(response.body(),
+                        new TypeToken<ApiResponse<List<Inventaris>>>() {}.getType());
+
+        if (!apiResp.success)
+            throw new Exception(apiResp.message);
+
+        return apiResp.data;
+    }
+
+    public void create(Inventaris i) throws Exception {
+        String json = gson.toJson(i);
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(BASE_URL))
+                .header("Content-Type", "application/json")
+                .POST(HttpRequest.BodyPublishers.ofString(json))
+                .build();
+
+        HttpResponse<String> response =
+                client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        handleResponse(response);
+    }
+
+    public void update(Inventaris i) throws Exception {
+        var requestBody = new HashMap<String, Object>();
+        requestBody.put("nama_barang", i.getNamaBarang());
+        requestBody.put("kategori", i.getKategori());
+        requestBody.put("kondisi", i.getKondisi());
+        requestBody.put("jumlah", i.getJumlah());
+
+        String json = gson.toJson(requestBody);
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(BASE_URL + "/" + i.getId()))
+                .header("Content-Type", "application/json")
+                .PUT(HttpRequest.BodyPublishers.ofString(json))
+                .build();
+
+        HttpResponse<String> response =
+                client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        handleResponse(response);
+    }
+
+    public void delete(int id) throws Exception {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(BASE_URL + "/" + id))
+                .DELETE()
+                .build();
+
+        HttpResponse<String> response =
+                client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        handleResponse(response);
+    }
+
+    private static class ApiResponse<T> {
+        boolean success;
+        T data;
+        String message;
+    }
+
+    private void handleResponse(HttpResponse<String> response) throws Exception {
+        if (response.statusCode() < 200 || response.statusCode() >= 300) {
+            throw new RuntimeException("HTTP " + response.statusCode() + ": " + response.body());
+        }
+
+        ApiResponse<?> apiResp = gson.fromJson(response.body(), ApiResponse.class);
+        if (!apiResp.success)
+            throw new Exception(apiResp.message);
+    }
+}
---
+++ src/model/Inventaris.java
@@ -0,0 +1,53 @@
+package model;
+
+public class Inventaris {
+
+    private int id;
+    private String namaBarang;
+    private String kategori;
+    private String kondisi;
+    private int jumlah;
+
+    public Inventaris() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNamaBarang() {
+        return namaBarang;
+    }
+
+    public void setNamaBarang(String namaBarang) {
+        this.namaBarang = namaBarang;
+    }
+
+    public String getKategori() {
+        return kategori;
+    }
+
+    public void setKategori(String kategori) {
+        this.kategori = kategori;
+    }
+
+    public String getKondisi() {
+        return kondisi;
+    }
+
+    public void setKondisi(String kondisi) {
+        this.kondisi = kondisi;
+    }
+
+    public int getJumlah() {
+        return jumlah;
+    }
+
+    public void setJumlah(int jumlah) {
+        this.jumlah = jumlah;
+    }
+}
